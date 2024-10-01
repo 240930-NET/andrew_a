aa990ad5390edddc4030efd9046a832bd3325ed3
Guwahavel <andrewangaroni@outlook.com>
2024-10-01 13:28:00 -0500

    Simple console program: Zero or One

---
+++ zero_or_one/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace zero_or_one;
+
+class Program
+{   
+    static void Main(string[] args)
+    {   
+        int zeroes = 0;
+        int ones = 0;
+
+        Console.WriteLine("Are you a Zero or a One? Input your favorite number to find out:");
+        foreach (char c in Console.ReadLine()) {
+            switch(c) {
+                case '0': 
+                    zeroes++;
+                    continue;
+                case '1':
+                    ones++;
+                    continue;
+            }
+        }
+
+        if (zeroes != ones) {
+            if (zeroes > ones) {
+                Console.WriteLine("You are a Zero.");
+            } else if (ones > zeroes) {
+                Console.WriteLine("You are a One.");
+            }
+        } else {
+            Console.WriteLine("You are neither a Zero nor a One.");
+        }
+    }
+}
