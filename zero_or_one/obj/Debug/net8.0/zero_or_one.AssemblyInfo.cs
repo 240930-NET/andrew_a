1e0f9a4dcfaad5073939a82d70eda7009185d5f6
Guwahavel <andrewangaroni@outlook.com>
2024-10-16 10:59:31 -0500

    EFTest from yesterday

--- zero_or_one/obj/Debug/net8.0/zero_or_one.AssemblyInfo.cs
+++ zero_or_one/obj/Debug/net8.0/zero_or_one.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zero_or_one")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f24ac23964c104b25618e93273ce0028d96536b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a0a82e0b3cf312afa867cfd1a6ae6daf895d81f")]
 [assembly: System.Reflection.AssemblyProductAttribute("zero_or_one")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zero_or_one")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
